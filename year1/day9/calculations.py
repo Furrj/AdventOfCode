9e09d837453f748db516dac28b485610b0cd2c17
Jackson Furr <jackson.a.furr@gmail.com>
2023-10-15 14:21:10 -0400

    year1 day9 ip

--- year1/day9/calculations.py
+++ year1/day9/calculations.py
@@ -1,12 +1,37 @@
-def calculate_routes(
-    cities: list[str], distances: list[tuple[str, str, int]]
+def calculate_distances(
+    cities: list[str], distances: list[tuple[str, str, int]], routes: list[list[str]]
 ) -> list[tuple[str, int]]:
-    routes: list[tuple[str, int]] = []
-    start: str = ""
-    route: list[str] = []
-    distance: int = 0
+    route_distances: list[tuple[str, int]] = []
+
+    return route_distances
+
+
+def generate_perms(
+    cities: list[str], route: list[str] = [], routes: list[list[str]] = []
+) -> list[list[str]]:
+    if len(cities) == 0:
+        routes.append(route.copy())
 
     for city in cities:
-        start = city
+        route.append(city)
+        choices: list[str] = cities.copy()
+        choices.remove(city)
+        generate_perms(cities=choices, route=route, routes=routes)
+        route.pop()
 
     return routes
+
+
+def valid_routes(
+    distances: list[tuple[str, str, int]], routes: list[list[str]]
+) -> list[list[str]]:
+    valid_routes: list[list[str]] = []
+
+    for route in routes:
+        for i in range(len(route) - 1):
+            valid: bool = False
+            for d in distances:
+                if route[i] in distances and route[i + 1] in distances:
+                    valid = True
+
+    return valid_routes
--- year1/day9/main.py
+++ year1/day9/main.py
@@ -3,12 +3,14 @@ import calculations as c
 
 
 def main() -> None:
-    lines: list[str] = p.read_file("inputs/test_input.txt")
+    lines: list[str] = p.read_file("inputs/input.txt")
     cities: list[str] = p.get_cities(lines)
     distances: list[tuple[str, str, int]] = p.get_distances(lines)
 
-    routes = c.calculate_routes(cities, distances)
-    print(distances)
+    routes = c.generate_perms(cities=cities)
+    valid_routes = c.valid_routes(distances, routes)
+    # for v in valid_routes:
+    #     print(v)
 
 
 main()
